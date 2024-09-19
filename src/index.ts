d985d26a9a1829e5461689c55c22b558b345386e
Luis Edward Miranda <luisedward.miranda@gmail.com>
2024-09-19 20:24:40 +0800

    chore: move the App entry

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
 import App from '../App';
-
-// Import your components to export
-export { MyButton as Button } from './components/Button/Button';
-// Add more component exports as needed
-
 registerRootComponent(App);
+
+export { MyButton as Button } from './components/Button/Button';
