5448b913990ee15d9fbb1a4b8417c07bfa56675f
Luis Edward Miranda <luisedward.miranda@gmail.com>
2024-09-20 19:03:38 +0800

    feat: expose the profile tile and shimmer component

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,9 @@
 export { default as TextBetweenLine } from './components/TextBetweenLine';
 export { default as Notice } from './components/Notice';
+export { default as ProfileTile } from './components/ProfileTile';
+export { default as Shimmer } from './components/Shimmer';
 export * from './components/Notice.types';
 export * from './components/TextBetweenLine.types';
+export * from './components/ProfileTile.types';
+export * from './components/Shimmer.types';
 //# sourceMappingURL=index.js.map
--- docs/src/index.ts
+++ docs/src/index.ts
@@ -1,6 +1,10 @@
 
 export { default as TextBetweenLine } from './components/TextBetweenLine';
 export { default as Notice } from './components/Notice';
+export { default as ProfileTile } from './components/ProfileTile';
+export { default as Shimmer } from './components/Shimmer';
 
 export * from './components/Notice.types';
 export * from './components/TextBetweenLine.types';
+export * from './components/ProfileTile.types';
+export * from './components/Shimmer.types';
